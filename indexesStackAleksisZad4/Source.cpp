45a2dda6557a5ea65512a690c8e4543ac94d1227
Valyo0430 <72649987+zzvalkatazz@users.noreply.github.com>
2024-07-09 16:57:06 +0300

    Add files via upload

---
+++ indexesStackAleksisZad4/Source.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+
+size_t findSquare(std::vector<size_t> input)
+{
+	size_t result = 0;
+	std::stack<size_t> indexes;
+	input.push_back(0);
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		while (!indexes.empty() && input[indexes.top()] >= input[i])
+		{
+			size_t currHeight = input[indexes.top()];
+			indexes.pop();
+			size_t currWidth = indexes.empty() ? i : i - indexes.top() - 1;
+			result = std::max(result, currHeight * currWidth);
+		}
+		indexes.push(i);
+	}
+	return result;
+}
