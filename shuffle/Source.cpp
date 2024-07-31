2e70c7e6bbbb330b6672d018f86238deea52b6e8
Valyo0430 <72649987+zzvalkatazz@users.noreply.github.com>
2024-08-01 00:34:55 +0300

    Add files via upload

---
+++ shuffle/Source.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+
+
+template<typename T>
+struct Node
+{
+	T data;
+	Node* next;
+
+	Node(T data,Node* next=nullptr) : data(data),next(next){}
+};
+template<typename T>
+Node<T>* getSecondHalf(Node<T>* list)
+{
+	Node<T>* slow = list;
+	Node<T>* fast = list;
+
+	while (fast->next && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	Node<T>* secondHalf = slow->next;
+	slow->next = nullptr;
+	return secondHalf;
+}
+template<typename T>
+Node<T>* reverse(Node<T>*& head)
+{
+	Node<T>* prev = nullptr;
+	Node<T>* current = head;
+
+	while (current)
+	{
+		Node<T>* next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	head = prev;
+	return head;
+}
+template<typename T>
+Node<T>* shuffle(Node<T>* head)
+{
+	Node<T>* secondHalf = getSecondHalf(head);
+	Node<T>* end = secondHalf;
+	secondHalf=reverse(secondHalf);
+	end->next = head;
+	return secondHalf;
+}
+void print(Node<int>* head)
+{
+	while (head)
+	{
+		std::cout << head->data << ' ';
+		head = head->next;
+	}
+}
+int main()
+{
+	Node<int>* head = new Node<int>(1, new Node<int>(2, new Node<int>(3, new Node<int>(4, new Node<int>(5)))));
+	Node<int>* head2 = new Node<int>(1, new Node<int>(2, new Node<int>(3, new Node<int>(4, new Node<int>(5,
+		new Node<int>(6, new Node<int>(7, new Node<int>(8, new Node<int>(9, new Node<int>(10))))))))));
+
+
+	Node<int>* newHead = shuffle(head);
+	print(newHead);
+
+	Node<int>* newHead2 = shuffle(head2);
+	std::cout << '\n';
+	print(newHead2);
+}
